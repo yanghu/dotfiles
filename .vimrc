f266df76b7528c1554d1e3206633f536a531307b
Yang Hu <hueyhy@gmail.com>
2015-07-12 15:05:33 -0400

    add vimrc and snippets and vim initial script

---
+++ .vimrc
@@ -0,0 +1,89 @@
+let g:pathogen_disabled = ["vim-easytags"]
+execute pathogen#infect()
+execute pathogen#helptags()
+syntax on
+filetype on
+filetype plugin indent on
+
+set background=dark
+colorscheme solarized
+set number
+
+set foldmethod=indent
+set foldlevel=99
+
+" Only do this part when compiled with support for autocommands.
+if has("autocmd")
+    " Use filetype detection and file-based automatic indenting.
+    filetype plugin indent on
+
+    " Use actual tab chars in Makefiles.
+    autocmd FileType make set tabstop=8 shiftwidth=8 softtabstop=0 noexpandtab
+endif
+
+" For everything else, use a tab width of 4 space chars.
+set tabstop=4       " The width of a TAB is set to 4.
+                    " Still it is a \t. It is just that
+                    " Vim will interpret it to be having
+                    " a width of 4.
+set shiftwidth=4    " Indents will have a width of 4.
+set softtabstop=4   " Sets the number of columns for a TAB.
+set expandtab       " Expand TABs to spaces.
+
+
+au FileType python set omnifunc=pythoncomplete#Complete
+"let g:SuperTabDefaultCompletionType = \"context\"
+"set completeopt=menuone,longest,preview
+
+
+let g:pymode_lint_sort = ['E', 'C', 'I']
+map <F7> :PymodeLint<CR>
+
+map <c-j> <c-w>j
+map <c-k> <c-w>k
+map <c-l> <c-w>l
+map <c-h> <c-w>h
+
+nnoremap <F3> :NumbersToggle<CR>
+nnoremap <F4> :NumbersOnOff<CR>
+
+nnoremap <F5> :GundoToggle<CR>
+nmap <F8> :TagbarToggle<CR>
+
+" make YCM compatible with UltiSnips (using supertab)
+let g:ycm_key_list_select_completion = ['<C-n>', '<Down>']
+let g:ycm_key_list_previous_completion = ['<C-p>', '<Up>']
+
+let g:UltiSnipsExpandTrigger="<tab>"
+let g:UltiSnipsJumpForwardTrigger="<c-j>"
+let g:UltiSnipsJumpBackwardTrigger="<c-b>"
+
+let g:UltiSnipsEditSplit="vertical"
+
+
+let g:airline#extensions#tabline#enabled = 1
+
+let g:ctrlp_working_path_mode = 'ra'
+set wildignore+=*/tmp/*,*.so,*.swp,*.zip
+set wildignore+=*\\tmp\\*,*.swp,*.zip,*.exe  
+"let g:ctrlp_custom_ignore = '\v[\/]\.(git|hg|svn)$'
+let g:ctrlp_custom_ignore = {
+\ 'dir':  '\v[\/]\.(git|hg|svn)$',
+\ 'file': '\v\.(exe|so|dll)$',
+\ 'link': 'some_bad_symbolic_links',
+\ }
+
+
+let g:syntastic_always_populate_loc_list = 1
+let g:syntastic_auto_loc_list = 1
+let g:syntastic_check_on_open = 1
+let g:syntastic_check_on_wq = 0
+
+nnoremap <C-]> g<C-]>
+let g:easytags_on_cursorhold = 0
+let g:easytags_auto_highlight = 0
+
+if &shell =~# 'bin/fish$'
+    set shell=/bin/sh
+endif
+
---
+++ vim_init.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+#this script is originated from 
+#http://sontek.net/blog/detail/turning-vim-into-a-modern-python-ide
+#© John Anderson <sontek@gmail.com> 2011
+#usage:
+#for pydoc.  use <leader>pw  when cursor on a word
+#for flake8, press F7
+#for 
+ 
+#TODO: install ctags for tagbar
+#http://majutsushi.github.io/tagbar/
+sudo apt-get install ctags
+ 
+#install flake8
+sudo pip install flake8
+  
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+cd ~/.vim/bundle
+#sensible
+git clone git://github.com/tpope/vim-sensible.git
+#numbers.vim
+git clone https://github.com/myusuf3/numbers.vim
+#airline sattus bar. Remember to run :Helptags to generate help tags
+git clone https://github.com/bling/vim-airline
+vim -u NONE -c "helptags vim-airline/doc" -c q
+git clone https://github.com/kien/ctrlp.vim
+git clone https://github.com/easymotion/vim-easymotion
+git clone https://github.com/tpope/vim-fugitive
+vim -u NONE -c "helptags vim-fugitive/doc" -c q
+git clone https://github.com/airblade/vim-gitgutter
+git clone https://github.com/majutsushi/tagbar.git
+#color
+git clone git://github.com/altercation/vim-colors-solarized.git
+#inde guides. default toggle mapping: <leader>ig
+git clone git://github.com/nathanaelkane/vim-indent-guides.git
+#syntax check. run :SyntasticInfo to see running checkers.
+# with "unimpared", use "[l" and "]l" to go through errors
+#lclose to close it 
+git clone https://github.com/scrooloose/syntastic.git
+vim -u NONE -c "helptags syntastic/doc" -c q
+#toggle curser for insert mode
+git clone https://github.com/jszakmeister/vim-togglecursor
+
+git clone https://github.com/Valloric/YouCompleteMe.git
+cd YouCompleteMe
+git submodule update --init --recursive
+./install.sh --clang-completer --gocode-completer
+#for fish shell, do this
+#cat "set -x DYLD_FORCE_FLAT_NAMESPACE 1" >> ~/.config/fish/config.fish
+
+cd ~/.vim/bundle
+git clone https://github.com/SirVer/ultisnips
+git clone https://github.com/honza/vim-snippets
+git clone git://github.com/tpope/vim-surround.git
+git clone https://github.com/Raimondi/delimitMate
+
+git clone https://github.com/scrooloose/nerdcommenter
+vim -u NONE -c "helptags nerdcommenter/doc" -c q
+
+git clone git://github.com/tpope/vim-unimpaired.git
+
+git clone https://github.com/sjl/gundo.vim
+
+git clone https://github.com/xolox/vim-easytags
+git clone https://github.com/xolox/vim-misc
+
+git clone https://github.com/klen/python-mode.git
+
+
+cd ~/.vim
+git submodule init
+git submodule update
+git submodule foreach git submodule init
+git submodule foreach git submodule update
+
+
+mv ~/.vimrc ~/.vimrc.orig
+cp .vimrc ~/.vimrc
