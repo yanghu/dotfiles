88177ec6303e38e28aa6bed409ff09c0a2e3c992
Yang Hu <yang@yhu.me>
2024-05-06 09:23:26 -0700

    add wezterm config

---
+++ wezterm/keymap.lua
@@ -0,0 +1,46 @@
+local M = {}
+
+local wezterm = require("wezterm")
+local act = wezterm.action
+M.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
+
+M.keymaps = {
+	{
+		key = "-",
+		mods = "LEADER",
+		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "|",
+		mods = "LEADER|SHIFT",
+		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "h",
+		mods = "LEADER",
+		action = act.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "l",
+		mods = "LEADER",
+		action = act.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "j",
+		mods = "LEADER",
+		action = act.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "k",
+		mods = "LEADER",
+		action = act.ActivatePaneDirection("Up"),
+	},
+	-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+	{
+		key = "a",
+		mods = "LEADER|CTRL",
+		action = act.SendKey({ key = "a", mods = "CTRL" }),
+	},
+}
+
+return M
