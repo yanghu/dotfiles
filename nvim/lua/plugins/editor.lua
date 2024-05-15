109cd1ba64bbda777fdf2daa1b4255beaebf8842
Yang Hu <yang@yhu.me>
2024-05-14 22:39:43 -0700

    Add nvim-tmux-navigator integration.
    Now use c-hjkl to move between vim and tmux windows seamlessly.

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -563,6 +563,24 @@ return {
 	}, -- }}}
 	{ "tpope/vim-sleuth", lazy = false },
 
+	{ -- alexghergh/nvim-tmux-navigation {{{2
+		"alexghergh/nvim-tmux-navigation",
+		config = function()
+			local nvim_tmux_nav = require("nvim-tmux-navigation")
+
+			nvim_tmux_nav.setup({
+				disable_when_zoomed = true, -- defaults to false
+			})
+
+			vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+			vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+			vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+			vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+			vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+			-- vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+		end,
+		event = "VeryLazy",
+	}, -- }}}
 	-- Sessions
 	{ -- olimorris/persisted.nvim {{{
 		"olimorris/persisted.nvim",
