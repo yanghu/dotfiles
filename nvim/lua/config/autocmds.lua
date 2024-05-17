992d4df59e6752075bb068674a0144b09f016565
Yang Hu <yang@yhu.me>
2024-05-16 22:07:42 -0700

    Add autocmd to auto toggle searchHL.
    Based on solution from https://github.com/ibhagwan/nvim-lua/blob/b2b15c1304558bff54fddaf03265b3cc700c4164/lua/autocmd.lua#L111
    
    See https://www.reddit.com/r/neovim/comments/1ct2w2h/lua_adaptation_of_vimcool_auto_nohlsearch/

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -110,3 +110,49 @@ augroup("auto_create_dir", function(g)
 		end,
 	})
 end)
+
+--  source: https://github.com/ibhagwan/nvim-lua/blob/b2b15c1304558bff54fddaf03265b3cc700c4164/lua/autocmd.lua#L111
+--  Automatically toggle search HL. See also https://www.reddit.com/r/neovim/comments/1ct2w2h/lua_adaptation_of_vimcool_auto_nohlsearch/
+augroup("ibhagwan/ToggleSearchHL", function(g)
+	aucmd("InsertEnter", {
+		group = g,
+		callback = function()
+			vim.schedule(function()
+				vim.cmd("nohlsearch")
+			end)
+		end,
+	})
+	aucmd("CursorMoved", {
+		group = g,
+		callback = function()
+			-- No bloat lua adpatation of: https://github.com/romainl/vim-cool
+			local view, rpos = vim.fn.winsaveview(), vim.fn.getpos(".")
+			assert(view.lnum == rpos[2])
+			assert(view.col + 1 == rpos[3])
+			-- Move the cursor to a position where (whereas in active search) pressing `n`
+			-- brings us to the original cursor position, in a forward search / that means
+			-- one column before the match, in a backward search ? we move one col forward
+			vim.cmd(
+				string.format(
+					"silent! keepjumps go%s",
+					(vim.fn.line2byte(view.lnum) + view.col + 1 - (vim.v.searchforward == 1 and 2 or 0))
+				)
+			)
+			-- Attempt to goto next match, if we're in an active search cursor position
+			-- should be equal to original cursor position
+			local ok, _ = pcall(vim.cmd, "silent! keepjumps norm! n")
+			local insearch = ok
+				and (function()
+					local npos = vim.fn.getpos(".")
+					return npos[2] == rpos[2] and npos[3] == rpos[3]
+				end)()
+			-- restore original view and position
+			vim.fn.winrestview(view)
+			if not insearch then
+				vim.schedule(function()
+					vim.cmd("nohlsearch")
+				end)
+			end
+		end,
+	})
+end)
