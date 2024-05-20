c7ca4c54d60aa37120c87c92eb930d55cd4c84d0
Yang Hu <huyang@google.com>
2024-05-20 15:18:48 -0700

    add helper to check at_work environment. update cmp pluginto be compatible with work plugin

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -111,7 +111,25 @@ return {
 			}, -- }}}
 			"saadparwaiz1/cmp_luasnip",
 
-			"hrsh7th/cmp-cmdline",
+			{
+				"hrsh7th/cmp-cmdline",
+				dependencies = { "hrsh7th/nvim-cmp" },
+				keys = { "/", "?", ":" },
+				config = function()
+					local cmp = require("cmp")
+					-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+					cmp.setup.cmdline(":", {
+						mapping = cmp.mapping.preset.cmdline(),
+						sources = cmp.config.sources({
+							{ name = "path" },
+						}, {
+							{ name = "cmdline", option = {
+								ignore_cmds = { "Man", "!" },
+							} },
+						}),
+					})
+				end,
+			},
 			-- Adds other completion capabilities.
 			--  nvim-cmp does not ship with all sources by default. They are split
 			--  into multiple repos for maintenance purposes.
@@ -121,25 +139,10 @@ return {
 			-- Adds pictograms to neovim built-in lsp
 			"onsails/lspkind.nvim",
 		},
-		config = function()
-			-- See `:help cmp`
+		opts = function(_, opts)
 			local cmp = require("cmp")
 			local luasnip = require("luasnip")
-			luasnip.config.setup({})
-			cmp.setup.cmdline(":", {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = cmp.config.sources({
-					{ name = "path" },
-				}, {
-					{
-						name = "cmdline",
-						option = {
-							ignore_cmds = { "Man", "!" },
-						},
-					},
-				}),
-			})
-			cmp.setup({
+			return vim.tbl_deep_extend("force", opts, {
 				snippet = {
 					expand = function(args)
 						luasnip.lsp_expand(args.body)
@@ -208,16 +211,29 @@ return {
 					{ name = "buffer" },
 				},
 				formatting = {
-					format = require("lspkind").cmp_format({
+					format = {
 						mode = "symbol_text", -- show only symbol annotations
 						maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
 						-- can also be a function to dynamically calculate max width such as
 						-- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
 						ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
 						show_labelDetails = true, -- show labelDetails in menu. Disabled by default
-					}),
+					},
 				},
 			})
+		end,
+		config = function(_, opts)
+			-- See `:help cmp`
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			luasnip.config.setup({})
+			local lspkind = require("lspkind")
+
+			cmp.setup(vim.tbl_deep_extend("force", opts, {
+				formatting = {
+					format = lspkind.cmp_format(opts.formatting.format),
+				},
+			}))
 		end,
 	}, -- }}}
 	--
