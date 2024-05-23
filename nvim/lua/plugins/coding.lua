90dd4937e6e762cbc9d48188b2b0f29ae31ceeb4
Yang Hu <huyang@google.com>
2024-05-23 00:44:20 -0700

    limit lsp cmp suggestions

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -205,7 +205,7 @@ return {
 					--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
 				}), -- }}}
 				sources = {
-					{ name = "nvim_lsp" },
+					{ name = "nvim_lsp" , max_item_count = 8 },
 					{ name = "luasnip" },
 					{ name = "path" },
 					{ name = "buffer", max_item_count = 5 },
